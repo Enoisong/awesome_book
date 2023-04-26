bb7395ff41974625899fce1bf34ec780db1a58b5
Enoisong <enobongitiaba988@gmail.com>
2023-04-26 14:40:51 -0700

    refactor app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bookTitle = document.getElementById('bookTitle');
 const bookAuthors = document.getElementById('bookAuthors');
 const AwsomeBkForm = document.getElementById('AwsomeBkForm');
 const allbooks = document.getElementById('BookRecord');
+
 class Book {
   constructor(bookTitle, bookAuthor) {
     this.bookTitle = bookTitle;
@@ -10,56 +11,69 @@ class Book {
   }
 }
 
-const mybooks = JSON.parse(localStorage.getItem('bookArchive')) || [];
+class bookRack {
+  constructor() {
+    this.booksrecord = JSON.parse(localStorage.getItem('bookArchive')) || [];
+  }
 
-// function to display books
-function displayBook(bookTitle, bookAuthor, id) {
-  const templateHTML = `${bookTitle}<br> ${bookAuthor}<br>
+ 
+static displayBook(bookTitle, bookAuthor, id) {
+  const templateHTML = `
+  <tr>
+  <td class="table-item"><p>"${bookTitle}" by ${bookAuthor}</p>
     <button type='button' class="remove" id="${id}">Remove</button>
-    <hr>`;
+    </td>
+    </tr>
+    `;
+    
   allbooks.insertAdjacentHTML('beforeend', templateHTML);
 }
 
-function getBooks() {
+static getBooks() {
   const storage = JSON.parse(localStorage.getItem('bookArchive'));
   storage.forEach((book) => {
-    displayBook(book.bookTitle, book.bookAuthor, book.id);
+    bookRack.displayBook(book.bookTitle, book.bookAuthor, book.id);
   });
 }
 
-// add book functionality
-function addBook() {
+remove(button, key) {  
+button.addEventListener('click', () => {
+  if (key === 0) {
+    this. booksrecord.splice(key, key + 1);
+  } else {
+    this.booksrecord.splice(key, 1);
+  }
+
+  allbooks.innerHTML = '';
+  localStorage.setItem('bookArchive', JSON.stringify(this.booksrecord));
+  window.location.reload();
+  localStorage.setItem('bookArchive', JSON.stringify(this.booksrecord));
+});
+
+ }
+
+ addBook() {
   AwsomeBkForm.addEventListener('submit', () => {
     const inputBook = new Book(bookTitle.value, bookAuthors.value);
-    mybooks.push(inputBook);
+    this.booksrecord.push(inputBook);
+    localStorage.setItem('bookArchive', JSON.stringify(this.booksrecord));
     bookTitle.value = '';
     bookAuthors.value = '';
     bookTitle.focus();
     bookAuthors.focus();
-    localStorage.setItem('bookArchive', JSON.stringify(mybooks));
-    // add book to DOM
-    getBooks();
+    bookRack.getBooks ();
   });
-
-  localStorage.setItem('bookArchive', JSON.stringify(mybooks));
-  getBooks();
+    localStorage.setItem('bookArchive', JSON.stringify(this.booksrecord));
+   bookRack.getBooks();   
+  }  
 }
 
-addBook();
+const keep = new bookRack();
+keep.addBook();
 
-// remove book functionality
 const removeAction = document.querySelectorAll('.remove');
 removeAction.forEach((btn, index) => {
-  btn.addEventListener('click', () => {
-    if (index === 0) {
-      mybooks.splice(index, index + 1);
-    } else {
-      mybooks.splice(index, index);
-    }
-
-    allbooks.innerHTML = '';
-    localStorage.setItem('bookArchive', JSON.stringify(mybooks));
-    window.location.reload();
-    localStorage.setItem('bookStorage', JSON.stringify(mybooks));
-  });
+  keep.remove(btn, index);
 });
+
+ 
---
+++ book.js
@@ -0,0 +1,7 @@
+class Book {
+    constructor(bookTitle, bookAuthors) {
+      this.bookTitle = bookTitle;
+      this.bookAuthors = bookAuthors;
+    }
+}
+export default Book;

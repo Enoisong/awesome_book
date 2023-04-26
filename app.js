45136024070c58a877fa09fcf0357dfadb011029
Enoisong <enobongitiaba988@gmail.com>
2023-04-26 04:28:37 -0700

    setup

---
+++ app.js
@@ -0,0 +1,69 @@
+/*eslint-disabled*/
+
+let bookslist = [];
+
+const title = document.getElementById('book_title');
+const authors = document.getElementById('book_authors');
+const libraryForm = document.getElementById('AwsomeBkForm');
+const allbooks = document.getElementById('BookRecord');
+ 
+ 
+function displayBook(book_title, book_author, id) {
+    const templateHTML = `${book_title}<br> ${book_author}<br>
+    <button type='button' class="remove" id="${id}">Remove</button>
+    <hr>`;
+    allbooks.insertAdjacentHTML('beforeend', templateHTML);
+}
+class Book {
+    constructor(book_title, book_author) {
+        this.book_title = book_title;
+        this.book_author = book_author;       
+    }
+}
+
+//add function for local storage
+const mybooks = JSON.parse(localStorage.getItem('bookArchive')) || [];
+
+function getBooks() {
+    const storage = JSON.parse(localStorage.getItem('bookArchive'));
+    storage.forEach((book) => {
+        displayBook(book.book_title, book.book_author, book.id);
+    });
+}
+
+//add book functionality
+function addBook() {
+    AwsomeBkForm.addEventListener('submit', () => {
+        const inputBook = new Book(title.value, authors.value);
+        mybooks.push(inputBook);
+        book_title.value = '';
+        book_authors.value = '';
+        book_title.focus();
+        book_authors.focus();
+        localStorage.setItem('bookArchive', JSON.stringify(mybooks));
+        //add book to DOM
+        getBooks();
+    });
+
+    localStorage.setItem('bookArchive', JSON.stringify(mybooks));
+    getBooks();
+}
+
+addBook();
+
+//add remove book functionality
+const removeAction = document.querySelectorAll('.remove');
+removeAction.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        if (index === 0) {
+            mybooks.splice(index, index + 1);
+        } else {
+            mybooks.splice(index, index);
+        }
+
+        allbooks.innerHTML = '';
+        localStorage.setItem('bookArchive', JSON.stringify(mybooks));
+        window.location.reload();
+        localStorage.setItem('bookStorage', JSON.stringify(mybooks));
+    });       
+});
